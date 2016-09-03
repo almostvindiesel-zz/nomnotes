5e02695077a177c7199d8571b51d839672cc846f
almostvindiesel <john@marsland.org>
2016-09-03 10:21:44 +0200

    modified app.py for heroku deployment

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import os
+os.environ['NOMNOMTES_ENVIRONMENT'] = 'heroku'
+
+from nomnotes import app
 
 from flask import Flask
 app = Flask(__name__)
 
-from app import app
 from nomnotes import views
 
+
+"""
+from app import app
+from nomnotes import nomnotes
+import views
+"""
